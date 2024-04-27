f435aac66c84b17a4e7e5f55b8e46c83b85930e1
Frederic Latour <frederic.latour@enesyssoftware.com>
2024-04-27 19:52:35 +0200

    a020 wine_cellar

---
+++ lib/a020_wine_cellar.ex
@@ -0,0 +1,44 @@
+defmodule WineCellar do
+  def explain_colors do
+    [
+      white: "Fermented without skin contact.",
+      red: "Fermented with skin contact using dark-colored grapes.",
+      rose: "Fermented with some skin contact, but not enough to qualify as a red wine."
+    ]
+  end
+
+  def filter(cellar, color, opts \\ []) do
+    color_wines =
+      cellar |> Keyword.get_values(color)
+
+    Enum.reduce(opts, color_wines, fn {key, value}, wines ->
+      apply_filter(wines, key, value)
+    end)
+  end
+
+  defp apply_filter(wines, :year, year), do: filter_by_year(wines, year)
+  defp apply_filter(wines, :country, country), do: filter_by_country(wines, country)
+
+  # The functions below do not need to be modified.
+  defp filter_by_year(wines, year)
+  defp filter_by_year([], _year), do: []
+
+  defp filter_by_year([{_, year, _} = wine | tail], year) do
+    [wine | filter_by_year(tail, year)]
+  end
+
+  defp filter_by_year([{_, _, _} | tail], year) do
+    filter_by_year(tail, year)
+  end
+
+  defp filter_by_country(wines, country)
+  defp filter_by_country([], _country), do: []
+
+  defp filter_by_country([{_, _, country} = wine | tail], country) do
+    [wine | filter_by_country(tail, country)]
+  end
+
+  defp filter_by_country([{_, _, _} | tail], country) do
+    filter_by_country(tail, country)
+  end
+end
